3385786183216f113853343afa6bd17d52f98719
Dgichaba <dorothy1cherotich@gmail.com>
2022-10-28 17:32:48 +0300

    update files

--- js/form.js
+++ js/form.js
@@ -2,7 +2,15 @@
 function myFunc() {
     var dob=$("#birthday").val();
     var gender= $('input[name=gender]:checked', '#form1').val();
-    
+
+    if(dob===""){ // Validate Date of Birth Input Field
+        $("#error").html("Please enter Date of Birth");
+        $("#birthday").focus();
+    }else if(!($('input[name=gender]:checked').length > 0)){ // Validate Gender Input Field
+        $("#error").html("Please select Gender");
+    }else{
+        $("#error").html("");
+
     var dobparts = dob.split("-");
     var DD = parseInt(dobparts[2]);
     var MM = parseInt(dobparts[1]);
@@ -10,7 +18,7 @@ function myFunc() {
     var YY = parseInt(year.slice(2,4));
     var CC = parseInt(year.slice(0,2));
 
-    alert("DD- "+DD+" MM- "+MM+" YY- "+YY+" CC- "+CC)
+    //alert("DD- "+DD+" MM- "+MM+" YY- "+YY+" CC- "+CC)
     
     
     // Method to get the day of the week. 
@@ -18,15 +26,17 @@ function myFunc() {
     var day=(((CC/4)-2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7
 
     day=Math.floor(day)
-    alert(day);
+    //alert(day);
 
     if(gender==="male"){
         const name = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-
         document.getElementById("response").innerHTML="Your akan name is: " + name[day];
+        //$("#response").html("Your akan name is: " + name[day]); // Using jquery
 
     }else if(gender==="female"){
         const name = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
         document.getElementById("response").innerHTML="Your akan name is: " + name[day];
+        //$("#response").html("Your akan name is: " + name[day]); // Using jquery 
     }
+}
 }
